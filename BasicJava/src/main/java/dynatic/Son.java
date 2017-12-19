284f11015270c4b4ee709013334dadffc2470149
yantinggeng <tinggengyan@gmail.com>
2017-12-19 20:58:03 +0800

    kotilin

---
+++ BasicJava/src/main/java/dynatic/Son.java
@@ -0,0 +1,4 @@
+package dynatic;
+
+public class Son {
+}
---
+++ Kotlin/src/main/kotlin/NullTest.kt
@@ -0,0 +1,25 @@
+interface CanFly {
+    fun fly()
+}
+
+
+class Bird(f: CanFly) : CanFly by f {
+
+}
+
+class Bat : CanFly {
+    override fun fly() {
+        println("fly")
+    }
+
+    fun ff(): Unit {
+        println("ff")
+    }
+
+}
+
+fun main(args: Array<String>) {
+    var f: CanFly = Bat()
+    var b : CanFly = Bird(f)
+    b.fly()
+}
