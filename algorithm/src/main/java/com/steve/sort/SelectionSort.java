4100978bd26b012a820b9ba0268e23f54630b922
yantinggeng <tinggengyan@gmail.com>
2018-02-11 16:10:47 +0800

    [ algorithm ] 选择排序

--- algorithm/src/main/java/com/steve/sort/SelectionSort.java
+++ algorithm/src/main/java/com/steve/sort/SelectionSort.java
@@ -2,12 +2,12 @@ package com.steve.sort;
 
 /**
  * 在要排序的一组数中，选出最小的一个数与第一个位置的数交换；
+ * <p>
  * 然后在剩下的数当中再找最小的与第二个位置的数交换，如此循环到倒数第二个数和最后一个数比较为止。
- *
+ * <p>
  * Created by steveyan on 4/7/17.
  */
 public class SelectionSort {
-
     /**
      * 选择排序算法
      * 在未排序序列中找到最小元素，存放到排序序列的起始位置
@@ -32,4 +32,13 @@ public class SelectionSort {
             numbers[k] = temp;
         }
     }
+
+    public static void main(String[] args) {
+        int[] num = new int[]{7, 2, 1, 4, 8, 9, 5, 3, 6};
+        selectSort(num);
+        for (int i : num) {
+            System.out.println(i);
+        }
+    }
+
 }
